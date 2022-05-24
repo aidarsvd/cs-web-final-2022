01d4354ba8c75f35d09929b7006260671edcbf53
Aidar <achakiev2003@gmail.com>
2022-05-24 22:56:04 +0600

    inital

---
+++ src/main/java/pro/aidar/alatoonews/AlatooNewsApplication.java
@@ -0,0 +1,13 @@
+package pro.aidar.alatoonews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlatooNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlatooNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/aidar/alatoonews/AlatooNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.aidar.alatoonews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlatooNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
