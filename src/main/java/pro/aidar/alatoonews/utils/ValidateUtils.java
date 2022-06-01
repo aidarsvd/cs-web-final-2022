90daad350eff1080f3bd54f952d12fe53b21217f
Aidar <aisha.beishenova@alatoo.edu.kg>
2022-06-01 23:57:38 +0600

    fix password validator

--- src/main/java/pro/aidar/alatoonews/utils/ValidateUtils.java
+++ src/main/java/pro/aidar/alatoonews/utils/ValidateUtils.java
@@ -1,15 +1,8 @@
 package pro.aidar.alatoonews.utils;
 
-import java.util.regex.Pattern;
-
 public class ValidateUtils {
 
-    public static boolean isPasswordValid(String name){
-        return Pattern.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", name);
+    public static boolean isPasswordValid(String password){
+        return password.length() >= 6;
     }
-
-    public static boolean isUsernameValid(String username){
-        return Pattern.matches("^.{4,}$", username);
-    }
-
 }
