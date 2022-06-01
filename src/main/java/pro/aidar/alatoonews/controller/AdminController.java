2380683079e6ecec159ddfd5e250e03ce60e60bd
Aidar <aisha.beishenova@alatoo.edu.kg>
2022-06-01 21:03:13 +0600

    fix user avatar displaying in edit page

--- src/main/java/pro/aidar/alatoonews/controller/AdminController.java
+++ src/main/java/pro/aidar/alatoonews/controller/AdminController.java
@@ -2,11 +2,16 @@ package pro.aidar.alatoonews.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pro.aidar.alatoonews.model.entity.news.News;
 import pro.aidar.alatoonews.model.service.files.FileService;
 import pro.aidar.alatoonews.model.service.news.NewsService;
+import pro.aidar.alatoonews.model.service.user.UserService;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,15 +20,31 @@ public class AdminController {
 
     private final NewsService newsService;
     private final FileService fileService;
+    private final UserService userService;
 
     @GetMapping
-    public String getCreate() {
+    public String getCreate(
+            Principal principal,
+            Model model
+    ) {
+        model.addAttribute("user", userService.findByUsername(principal.getName()));
         return "add_news";
     }
 
-    @GetMapping
-    public String editNews() {
-        return "edit_news";
+    @GetMapping("/edit/{id}")
+    public String editNews(
+            @PathVariable Long id,
+            Model model,
+            Principal principal
+    ) {
+        Optional<News> byId = newsService.findById(id);
+        model.addAttribute("user", userService.findByUsername(principal.getName()));
+        if (byId.isPresent()) {
+            model.addAttribute("news", byId.get());
+            return "edit_news";
+        } else {
+            return "not_found";
+        }
     }
 
     @PostMapping("/create_news")
@@ -41,6 +62,26 @@ public class AdminController {
             news.setThumbnail(image);
         }
         newsService.save(news);
+        return "redirect:/";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String edit(
+            @PathVariable Long id,
+            @RequestParam MultipartFile thumbnail,
+            @RequestParam String title,
+            @RequestParam String content
+    ) {
+        News news = News.builder()
+                .id(id)
+                .title(title)
+                .content(content)
+                .build();
+        if (!thumbnail.isEmpty()) {
+            String image = fileService.save(thumbnail);
+            news.setThumbnail(image);
+        }
+        newsService.update(news);
         return "redirect:/";
     }
 
